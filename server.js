03e15a618b30614eac7ed27ec5a18c5d9af6b8a4
Jacob Leon Lyerla <umasour@yahoo.com>
2018-07-26 22:04:35 -0700

    created the server file for my back end

---
+++ server.js
@@ -0,0 +1,22 @@
+// import in modules
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { username, password } = require("./dontlook");
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+mongoose
+  .connect(`mongodb://${username}:${password}@ds255451.mlab.com:55451/ds`, { useNewUrlParser: true })
+  .catch(err => {
+    console.log("could not connect to mlab.com database", err);
+  });
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: `the api is running on port ${port}` });
+});
+
+const port = process.env.PORT || 5500;
+
+server.listen(port, () => console.log(`\n=== api is up on port: ${port} ==\n`));
