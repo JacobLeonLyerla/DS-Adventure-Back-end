ded1c2410e035909fb43b8dc2da925ad13cb55f9
Jacob Leon Lyerla <umasour@yahoo.com>
2018-07-27 02:28:39 -0700

    everything is coming together
    i need to make sure my  state is updating when i redirect to the log in page

--- Characters/player.js
+++ Characters/player.js
@@ -19,6 +19,10 @@ const Player = new mongoose.Schema({
      type:String,
      required:true
     },
+    gender:{
+        type:String,
+        defualt:"Unknown"
+    },
     health:{
         type:Number,
         default:100
