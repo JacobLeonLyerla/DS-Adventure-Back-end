16ad931cba779f38c68c3bcbe9a612ac549902e8
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-18 23:52:23 -0700

    setting up bycrpt for my model

--- Characters/player.js
+++ Characters/player.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const uniqueValidator = require("mongoose-unique-validator");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 11;
 const Player = new mongoose.Schema({
   name: {
     type: String,
@@ -162,5 +165,38 @@ const Player = new mongoose.Schema({
     default: false
   }
 });
+
+//Added in Bcrypt for PW hashing
+Player.pre("save", function(next) {
+  // check if record is new, fixes the issue with rehashing user's password on 
+  // each added letter that calls save()
+  if (!this.isModified("password")) return next();
+
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    return next();
+  });
+});
+
+Player.methods.checkPassword = function(plainTextPW, callback) {
+  return bcrypt.compare(plainTextPW, this.password, function(err, isValid) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isValid);
+  });
+};
+
+
+Player.methods.addLetter = function(letter_id) {
+  this.letters.push(letter_id);
+};
+
+Player.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", Player);
+
+
 
 module.exports = mongoose.model("Player", Player);
