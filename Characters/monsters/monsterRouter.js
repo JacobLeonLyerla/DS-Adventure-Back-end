7546c4415a3dd26a10ed86ace46c8c0794484c21
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:31:40 -0700

    monster router formatted for better readability

--- Characters/monsters/monsterRouter.js
+++ Characters/monsters/monsterRouter.js
@@ -1,56 +1,63 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Monster = require('./monster')
+const Monster = require("./monster");
 
-router.get("/:id",(req,res)=>{
-    const {id} = req.params
-    Monster
-    .findById(id).populate('items attacks')
-    .then(response=>{
-        res.status(202).json(response);
-    }).
-catch(err=>{
-    res.status(500).json
-})
-})
-router.put("/:id",(req,res)=>{
-    const{id} = req.params
-    const update = req.body
-    const options ={
-        new:true
-    }
-    Monster
-    .findByIdAndUpdate(id,update,options)
-    .then(response=>{
-        res.status(200).json(response)
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Monster.findById(id)
+    .populate("items attacks")
+    .then(response => {
+      res.status(202).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
+
+    .catch(err => {
+      res.status(500).json;
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const update = req.body;
+
+  const options = {
+    new: true
+  };
+
+  Monster.findByIdAndUpdate(id, update, options)
+    .then(response => {
+      res.status(200).json(response);
     })
-})
-router
-.get("/",(req,res)=>{
-    Monster
-    .find()
+
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/", (req, res) => {
+  Monster.find()
     .sort()
-    .then(response=>{
-        res.status(200).json(response)
+    .then(response => {
+      res.status(200).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
 
-router
-.post('/',(req,res)=>{
-    const monster = new Monster(req.body)
-    monster
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const monster = new Monster(req.body);
+  monster
     .save()
-    .then(response=>{
-        res.status(201).json(response)
+    .then(response => {
+      res.status(201).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
 module.exports = router;
