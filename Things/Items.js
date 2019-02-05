283043c2c3ca5568f9b9ba3ebb0a431841baab1d
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:29:44 -0700

    things item modal formatted for better readability

--- Things/Items.js
+++ Things/Items.js
@@ -1,50 +1,72 @@
 const mongoose = require("mongoose");
 
 const Item = new mongoose.Schema({
+
   name: {
     type: String,
+
     required: true
   },
+
   index: {
     type: Number,
+
     require: true,
+
     unique: true
   },
 
   slot:{
     type:String,
+
     required:true
   },
+
   health:{
     type:Number,
+
     default:0
   },
+
   endurance:{
     type:Number,
+
     default:0
   },
+
   strength:{
     type:Number,
+
     default:0
   },
+
   intellect:{
     type:Number,
+
     default:0
   },
+
   agility:{
     type:Number,
+
     default:0
   },
+
   healing:{
     type:Number,
+
     default:0
   },
+
   damage: {
     type:Number,
+
     default:0
   },
+
   rarity:{
     type:String,
+    
     default:"common"
   }
   
