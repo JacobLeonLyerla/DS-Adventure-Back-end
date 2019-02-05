abcc27ff0ccb8357e7349fcb8fd9a0e16436882e
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:30:42 -0700

    things items router formatted for better readability

--- Things/itemsRouter.js
+++ Things/itemsRouter.js
@@ -1,56 +1,59 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Items = require('./Items')
+const Items = require("./Items");
 
-router.get("/:id",(req,res)=>{
-    const {id} = req.params
-    Items
-    .findById(id)
-    .then(response=>{
-        res.status(202).json(response);
-    }).
-catch(err=>{
-    res.status(500).json
-})
-})
-router.put("/:id",(req,res)=>{
-    const{id} = req.params
-    const update = req.body
-    const options ={
-        new:true
-    }
-    Items
-    .findByIdAndUpdate(id,update,options)
-    .then(response=>{
-        res.status(200).json(response)
-    })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
-router
-.get("/",(req,res)=>{
-    Items
-    .find()
-    .sort('-created')
-    .then(response=>{
-        res.status(200).json(response)
-    })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-router
-.post('/',(req,res)=>{
-    const items = new Items(req.body)
-    items
+  Items.findById(id)
+    .then(response => {
+      res.status(202).json(response);
+    })
+    .catch(() => {
+      res.status(500).json;
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const update = req.body;
+
+  const options = {
+    new: true
+  };
+
+  Items.findByIdAndUpdate(id, update, options)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/", (req, res) => {
+  Items.find()
+    .sort("-created")
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const items = new Items(req.body);
+
+  items
     .save()
-    .then(response=>{
-        res.status(201).json(response)
+    .then(response => {
+      res.status(201).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
 module.exports = router;
