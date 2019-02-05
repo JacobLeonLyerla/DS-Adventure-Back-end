da8cfd3e9eefa07fc01bc916c230784e3cca0a69
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:31:17 -0700

    abilities attack router formatted for better readability

--- Characters/abilities/attackRouter.js
+++ Characters/abilities/attackRouter.js
@@ -1,56 +1,62 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Attack = require('./attack')
+const Attack = require("./attack");
 
-router.get("/:id",(req,res)=>{
-    const {id} = req.params
-    Attack
-    .findById(id).populate('items gear currentLocation')
-    .then(response=>{
-        res.status(202).json(response);
-    }).
-catch(err=>{
-    res.status(500).json
-})
-})
-router.put("/:id",(req,res)=>{
-    const{id} = req.params
-    const update = req.body
-    const options ={
-        new:true
-    }
-    Attack
-    .findByIdAndUpdate(id,update,options)
-    .then(response=>{
-        res.status(200).json(response)
-    })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
-router
-.get("/",(req,res)=>{
-    Attack
-    .find()
-    .sort('-created')
-    .then(response=>{
-        res.status(200).json(response)
-    })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-router
-.post('/',(req,res)=>{
-    const attack = new Attack(req.body)
-    attack
+  Attack.findById(id)
+    .populate("items gear currentLocation")
+    .then(response => {
+      res.status(202).json(response);
+    })
+    .catch(() => {
+      res.status(500).json;
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const update = req.body;
+
+  const options = {
+    new: true
+  };
+
+  Attack.findByIdAndUpdate(id, update, options)
+    .then(response => {
+      res.status(200).json(response);
+    })
+
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/", (req, res) => {
+  Attack.find()
+    .sort("-created")
+    .then(response => {
+      res.status(200).json(response);
+    })
+
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const attack = new Attack(req.body);
+  attack
     .save()
-    .then(response=>{
-        res.status(201).json(response)
+    .then(response => {
+      res.status(201).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
 module.exports = router;
