c3065c69f5251fac8c0283d1a84a4d4b39284077
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:31:06 -0700

    abilities attack modal formatted for better readability

--- Characters/abilities/attack.js
+++ Characters/abilities/attack.js
@@ -1,38 +1,51 @@
 const mongoose = require("mongoose");
+
 const Attack = new mongoose.Schema({
   name: {
     type: String,
+
     required: true,
+
     unique: true
   },
-  level:{
-      type:Number,
-      default:0
+
+  level: {
+    type: Number,
+
+    default: 0
   },
-cost:{
-    type:Number,
-    default:0
-},
-damage:{
-    type:Number,
-    default:0
-},
-leach:{
-    type:Number,
-    default:0
-},
-drain:{
-    type:Number,
-    defualt:0,
-},
-hitChance:{
-    type:Number,
-    default:25
-}
-,
-description:String,
 
+  cost: {
+    type: Number,
 
+    default: 0
+  },
+
+  damage: {
+    type: Number,
+
+    default: 0
+  },
+
+  leach: {
+    type: Number,
+
+    default: 0
+  },
+
+  drain: {
+    type: Number,
+
+    defualt: 0
+  },
+
+  hitChance: {
+    type: Number,
+
+    default: 25
+  },
+
+  description: String
 });
 
 module.exports = mongoose.model("Attack", Attack);
