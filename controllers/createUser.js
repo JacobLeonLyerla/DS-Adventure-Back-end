358403083daf1fdd3ea93cc1ba65ab62734d9403
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:28:39 -0700

    create user controller formatted for better readability

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,10 +1,13 @@
 const User = require("../Characters/player");
+
 const validateRegisterInput = require("../validation/register");
 
 const createUser = (req, res) => {
  
   const myNewUser = new User(req.body);
+
   const { errors, isValid } = validateRegisterInput(req.body);
+
   if (!isValid) return res.status(400).json(errors);
 
   myNewUser
@@ -12,8 +15,11 @@ const createUser = (req, res) => {
     .then(user => {
       res.status(201).json(user);
     })
+
     .catch(err => {
       res.status(500).json(err);
     });
+
 };
+
 module.exports = createUser;
