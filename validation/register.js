370228b4ca2dbca0cb6c5dfc0bfb42633db8af75
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:32:36 -0700

    validation register formatted for better readability

--- validation/register.js
+++ validation/register.js
@@ -1,25 +1,35 @@
 const Validator = require("validator");
+
 const isEmpty = require("./is-empty");
 
 const validateRegisterInput = ({ name, email, password, password2 }) => {
   let errors = {};
+
   name = !isEmpty(name) ? name : "";
+
   email = !isEmpty(email) ? email : "";
+
   password = !isEmpty(password) ? password : "";
+
   password2 = !isEmpty(password2) ? password2 : "";
+
   if (!Validator.isLength(name, { min: 2, max: 30 })) {
     errors.name =
       "name most be six or more characters, no more than thirty.";
   }
+
   if (Validator.isEmpty(name)) {
     errors.name = "Please enter a name.";
   }
+
   if (Validator.isEmpty(email)) {
     errors.email = "Please enter an Email.";
   }
+
   if (!Validator.isEmail(email)) {
     errors.email = "This email is not valid, please try another.";
   }
+
   if (Validator.isEmpty(password)) {
     errors.password = "Please enter a password.";
   }
@@ -28,6 +38,7 @@ const validateRegisterInput = ({ name, email, password, password2 }) => {
     errors.password =
       "Password most be six or more characters, no more than thirty.";
   }
+
   if (Validator.isEmpty(password2)) {
     errors.password2 = "Password must be confirmed.";
   }
@@ -42,4 +53,5 @@ const validateRegisterInput = ({ name, email, password, password2 }) => {
   };
 
 };
+
 module.exports = validateRegisterInput;
