63901248fc8d3d09133e552c0f9b1798c78f235e
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:27:39 -0700

    area modal formatted for readability

--- Locations/area.js
+++ Locations/area.js
@@ -1,50 +1,66 @@
 const mongoose = require("mongoose");
+
 const ObjectId = mongoose.Schema.Types.ObjectId;
+
 const Area = new mongoose.Schema({
   name: {
     type: String,
+
     required: true,
+
     unique: true
   },
-  monsters:[
+
+  monsters: [
     {
-    type:ObjectId,
-    ref:"Monster"
-  }
-],
+      type: ObjectId,
+
+      ref: "Monster"
+    }
+  ],
 
   items: [
     {
       type: ObjectId,
+
       ref: "Item"
     }
   ],
-  teir:{
-    type:Number,
-    default:1,
+
+  teir: {
+    type: Number,
+
+    default: 1
   },
 
   west: [
     {
       type: ObjectId,
-      ref: "Area",
+
+      ref: "Area"
     }
   ],
+
   east: [
     {
       type: ObjectId,
+
       ref: "Area"
     }
   ],
+
   north: [
     {
       type: ObjectId,
+
       ref: "Area"
     }
   ],
+
   south: [
     {
       type: ObjectId,
+
       ref: "Area"
     }
   ]
