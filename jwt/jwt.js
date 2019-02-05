46ec9400dd1da9a9b6937db2a289b8d08412192b
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:28:05 -0700

    jwt formatted for better readability

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -1,27 +1,30 @@
 const { ExtractJwt } = require("passport-jwt");
+
 const JwtStrategy = require("passport-jwt").Strategy;
+
 const passport = require("passport");
+
 const User = require("../Characters/player");
-const jwtOptions ={
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.key
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+  secretOrKey: process.env.key
 };
+
 const jwtStrategy = new JwtStrategy(jwtOptions, (payload, done) => {
-    User.findById(payload.sub)
+  User.findById(payload.sub)
     .select("-password")
-    .then(user=>{
-        if(user){
-            done(null,user);
-        }else{
-            done(null,false);
-        }
-        })
-        .catch(err=>
-            done(err,false)
-        )
-    });
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    })
+    .catch(err => done(err, false));
+});
 
-    const protected = passport.authenticate("jwt", { session: false });
+const protected = passport.authenticate("jwt", { session: false });
 
-    module.exports = { jwtStrategy, protected };
-    
+module.exports = { jwtStrategy, protected };
