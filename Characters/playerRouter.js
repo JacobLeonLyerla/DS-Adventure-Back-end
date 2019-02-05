7f3764badbad932ae16f44d74ff45bc434c777f5
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:32:02 -0700

    player router formatted for better readability

--- Characters/playerRouter.js
+++ Characters/playerRouter.js
@@ -1,60 +1,61 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Player = require('./player')
+const Player = require("./player");
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-
-
-
-router.get("/:id",(req,res)=>{
-    const {id} = req.params
-    Player
-    .findById(id).populate('items gear currentLocation attacks currentBattle')
-    .then(response=>{
-        res.status(202).json(response);
-    }).
-catch(err=>{
-    res.status(500).json(err)
-})
-})
-router.put("/:id",(req,res)=>{
-    const{id} = req.params
-    const update = req.body
-    const options ={
-        new:true
-    }
-    Player
-    .findByIdAndUpdate(id,update,options)
-    .then(response=>{
-        res.status(200).json(response)
+  Player.findById(id)
+    .populate("items gear currentLocation attacks currentBattle")
+    .then(response => {
+      res.status(202).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
-router
-.get("/",(req,res)=>{
-    Player
-    .find()
-    .sort('-created')
-    .then(response=>{
-        res.status(200).json(response)
-    })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
-router
-.post('/',(req,res)=>{
-    const player = new Player(req.body)
-    player
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const update = req.body;
+
+  const options = {
+    new: true
+  };
+
+  Player.findByIdAndUpdate(id, update, options)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/", (req, res) => {
+  Player.find()
+    .sort("-created")
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const player = new Player(req.body);
+
+  player
     .save()
-    .then(response=>{
-        res.status(201).json(response)
+    .then(response => {
+      res.status(201).json(response);
     })
-    .catch(err=>{
-        res.status(500).json({error:err})
-    })
-})
+
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
 module.exports = router;
