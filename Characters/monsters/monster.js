7556caf79c2b8774da517ca836149a8b7c62a607
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:31:30 -0700

    monster modal formatted for better readability

--- Characters/monsters/monster.js
+++ Characters/monsters/monster.js
@@ -1,46 +1,65 @@
 const mongoose = require("mongoose");
+
 const ObjectId = mongoose.Schema.Types.ObjectId;
+
 const Monster = new mongoose.Schema({
   name: {
     type: String,
+
     required: true,
+
     unique: true
   },
-  photo:String,
-  attacks:[{
-    type:ObjectId,
-    ref:"Attack"
-  }],
 
-level:{
-  type:Number,
-  default:1
-} ,
-rarity:{
-type:String,
-default:"common"
-},
-experience:{
-    type:Number,
-    default:0
-},
-health:{
-    type:Number,
-    default:100
-},
-endurance:{
-    type:Number,
-    default:100
-},
+  photo: String,
+
+  attacks: [
+    {
+      type: ObjectId,
+
+      ref: "Attack"
+    }
+  ],
+
+  level: {
+    type: Number,
+
+    default: 1
+  },
+
+  rarity: {
+    type: String,
+
+    default: "common"
+  },
+
+  experience: {
+    type: Number,
+
+    default: 0
+  },
+
+  health: {
+    type: Number,
+
+    default: 100
+  },
+  endurance: {
+    type: Number,
+
+    default: 100
+  },
   items: [
     {
       type: ObjectId,
+
       ref: "Item"
     }
   ],
-  idle:String,
-  battle:String
 
+  idle: String,
+
+  battle: String
 });
 
 module.exports = mongoose.model("Monster", Monster);
