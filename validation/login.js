0bdd8d146011af97f5a42fe612007117bca68455
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-04 23:32:28 -0700

    validation login  formatted for better readability

--- validation/login.js
+++ validation/login.js
@@ -1,23 +1,30 @@
 const Validator = require("validator");
+
 const isEmpty = require("../validation/is-empty");
 
-const validateLoginInput = ({username,password})=>{
-    let errros = {}
-    username = !isEmpty(username)? username:"";
-    password = !isEmpty(password)? password:"";
+const validateLoginInput = ({ username, password }) => {
+  let errros = {};
 
-    if(!Validator.isLength(username,{min:2, max:30})){
-        errros.username ="Username must be between two and thirty characters.";
-    }
-    if(Validator.isEmpty(username)){
-        errros.username = "You must enter a Password."
-    }
-    if(!Validator.isLength(password,{min:6,max:30})){
-        errros.password ="Password must be between two and thirty characters."
-    }
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    };
+  username = !isEmpty(username) ? username : "";
+
+  password = !isEmpty(password) ? password : "";
+
+  if (!Validator.isLength(username, { min: 2, max: 30 })) {
+    errros.username = "Username must be between two and thirty characters.";
+  }
+
+  if (Validator.isEmpty(username)) {
+    errros.username = "You must enter a Password.";
+  }
+
+  if (!Validator.isLength(password, { min: 6, max: 30 })) {
+    errros.password = "Password must be between two and thirty characters.";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
-module.exports = validateLoginInput
+
+module.exports = validateLoginInput;
