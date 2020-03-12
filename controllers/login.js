dbefe91d797126b2803ad18b66b3d03942f704a3
Jacob <jacobleonlyerla@gmail.com>
2020-03-11 21:16:07 -0700

    Formatting

--- controllers/login.js
+++ controllers/login.js
@@ -15,31 +15,54 @@ const makeToken = user => {
     email: user.email
   };
 
-  const options = { expiresIn: "2h" };
+  const options = {
+    expiresIn: "2h"
+  };
 
   return jwt.sign(payload, process.env.key, options);
 };
 
 const localStrategy = new LocalStrategy((name, password, done) => {
-  User.findOne({ name }, (err, user) => {
+  User.findOne({
+    name
+  }, (err, user) => {
     if (err) return done(err);
     if (!user) return done(null, false);
 
     user.checkPassword(password, (err, isMatch) => {
       if (err) return done(err);
       if (isMatch) {
-        const { _id, name, email, membership } = user;
-        return done(null, { _id, name, email, membership });
+        const {
+          _id,
+          name,
+          email,
+          membership
+        } = user;
+        return done(null, {
+          _id,
+          name,
+          email,
+          membership
+        });
       }
       return done(null, false);
     });
   });
 });
 
-const authenticate = passport.authenticate("local", { session: false });
+const authenticate = passport.authenticate("local", {
+  session: false
+});
 
 const login = (req, res) => {
-  res.json({ token: makeToken(req.user), user: req.user });
+  res.json({
+    token: makeToken(req.user),
+    user: req.user
+  });
 };
 
-module.exports = { login, authenticate, localStrategy };
+module.exports = {
+  login,
+  authenticate,
+  localStrategy
+};
