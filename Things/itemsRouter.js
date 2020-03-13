864999322fe686bca268990d442665f7a7b9adb8
Jacob <jacobleonlyerla@gmail.com>
2020-03-12 22:26:12 -0700

    Formatting

--- Things/itemsRouter.js
+++ Things/itemsRouter.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 const Items = require("./Items");
 
 router.get("/:id", (req, res) => {
-  const { id } = req.params;
+  const {
+    id
+  } = req.params;
 
   Items.findById(id)
     .then(response => {
@@ -15,7 +17,9 @@ router.get("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  const { id } = req.params;
+  const {
+    id
+  } = req.params;
 
   const update = req.body;
 
@@ -28,7 +32,9 @@ router.put("/:id", (req, res) => {
       res.status(200).json(response);
     })
     .catch(err => {
-      res.status(500).json({ error: err });
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
@@ -39,7 +45,9 @@ router.get("/", (req, res) => {
       res.status(200).json(response);
     })
     .catch(err => {
-      res.status(500).json({ error: err });
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
@@ -52,7 +60,9 @@ router.post("/", (req, res) => {
       res.status(201).json(response);
     })
     .catch(err => {
-      res.status(500).json({ error: err });
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
