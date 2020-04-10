0e68c0b307d8048c5fdc5948383e21c53c7296e3
Jacob <jacobleonlyerla@gmail.com>
2020-04-09 22:57:52 -0700

    formatting

--- controllers/login.js
+++ controllers/login.js
@@ -6,63 +6,61 @@ const passport = require("passport");
 
 const LocalStrategy = require("passport-local");
 
-const makeToken = user => {
+const makeToken = (user) => {
   const payload = {
     sub: user._id,
 
     name: user.name,
 
-    email: user.email
+    email: user.email,
   };
 
   const options = {
-    expiresIn: "2h"
+    expiresIn: "2h",
   };
 
   return jwt.sign(payload, process.env.key, options);
 };
 
 const localStrategy = new LocalStrategy((name, password, done) => {
-  User.findOne({
-    name
-  }, (err, user) => {
-    if (err) return done(err);
-    if (!user) return done(null, false);
-
-    user.checkPassword(password, (err, isMatch) => {
+  User.findOne(
+    {
+      name,
+    },
+    (err, user) => {
       if (err) return done(err);
-      if (isMatch) {
-        const {
-          _id,
-          name,
-          email,
-          membership
-        } = user;
-        return done(null, {
-          _id,
-          name,
-          email,
-          membership
-        });
-      }
-      return done(null, false);
-    });
-  });
+      if (!user) return done(null, false);
+
+      user.checkPassword(password, (err, isMatch) => {
+        if (err) return done(err);
+        if (isMatch) {
+          const { _id, name, email, membership } = user;
+          return done(null, {
+            _id,
+            name,
+            email,
+            membership,
+          });
+        }
+        return done(null, false);
+      });
+    }
+  );
 });
 
 const authenticate = passport.authenticate("local", {
-  session: false
+  session: false,
 });
 
 const login = (req, res) => {
   res.json({
     token: makeToken(req.user),
-    user: req.user
+    user: req.user,
   });
 };
 
 module.exports = {
   login,
   authenticate,
-  localStrategy
+  localStrategy,
 };
