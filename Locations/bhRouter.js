3f4d04ed571f6981fadd2fab9d42114eba63e52d
Jacob <jacobleonlyerla@gmail.com>
2020-04-10 22:14:24 -0700

    formatting routers

--- Locations/bhRouter.js
+++ Locations/bhRouter.js
@@ -3,50 +3,46 @@ const router = require("express").Router();
 const Area = require("./area");
 
 router.get("/:id", (req, res) => {
-  const {
-    id
-  } = req.params;
+  const { id } = req.params;
 
   Area.findById(id)
     .populate("items north south east west monsters")
-    .then(response => {
+    .then((response) => {
       res.status(202).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json;
     });
 });
 
 router.put("/:id", (req, res) => {
-  const {
-    id
-  } = req.params;
+  const { id } = req.params;
 
   const update = req.body;
 
   const options = {
-    new: true
+    new: true,
   };
 
   Area.findByIdAndUpdate(id, update, options)
-    .then(response => {
+    .then((response) => {
       res.status(200).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
-        error: err
+        error: err,
       });
     });
 });
 
 router.get("/", (req, res) => {
   Area.find()
-    .then(response => {
+    .then((response) => {
       res.status(200).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
-        error: err
+        error: err,
       });
     });
 });
@@ -56,12 +52,12 @@ router.post("/", (req, res) => {
 
   area
     .save()
-    .then(response => {
+    .then((response) => {
       res.status(201).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
-        error: err
+        error: err,
       });
     });
 });
